9527db9ad6c562c4ef7dcb06fe83993f21935b9f
achicote <ranaranitaranon@hotmail.com>
2023-12-19 12:54:54 +0100

    font awesome 6

--- UIAngular/Sicad/src/app/app.component.ts
+++ UIAngular/Sicad/src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Sicad';
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas, far);
+  }
 }
