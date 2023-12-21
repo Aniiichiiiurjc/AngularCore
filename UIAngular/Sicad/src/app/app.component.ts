574c5880ab78bec1e8337cbd6e3d7b8256750702
achicote <ranaranitaranon@hotmail.com>
2023-12-21 15:35:11 +0100

    routing

--- UIAngular/Sicad/src/app/app.component.ts
+++ UIAngular/Sicad/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FontAwesomeModule],
+  imports: [CommonModule, RouterOutlet, RouterLink, FontAwesomeModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- UIAngular/Sicad/src/app/list/list.component.ts
+++ UIAngular/Sicad/src/app/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-list',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-
+  route: ActivatedRoute = inject(ActivatedRoute);
+  listId: number;
+  constructor() {
+    this.listId = Number(this.route.snapshot.params['id'])?Number(this.route.snapshot.params['id']):-1;
+  }
 }
