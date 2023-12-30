8c78de12d3cad864c18eb360c2e359ab42f85ae6
achicote <ranaranitaranon@hotmail.com>
2023-12-30 12:12:57 +0100

    table y pagination con angular material

--- UIAngular/Sicad/src/app/search-engine/search-engine.component.ts
+++ UIAngular/Sicad/src/app/search-engine/search-engine.component.ts
@@ -1,18 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {Item} from '../item';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 @Component({
   selector: 'app-search-engine',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatPaginatorModule, MatTableModule],
   templateUrl: './search-engine.component.html',
   styleUrl: './search-engine.component.css'
 })
-export class SearchEngineComponent {
+export class SearchEngineComponent implements AfterViewInit{
   @Input() list!: Item[];
   Object = Object;
   // constructor() {
   //   //this.list = list;
   //   console.log(this.list);
   // }
+  dataSource!: MatTableDataSource<any>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  
+  ngAfterViewInit() {
+    this.dataSource = new MatTableDataSource<any>(this.list);
+    this.dataSource.paginator = this.paginator;
+  }
 }
