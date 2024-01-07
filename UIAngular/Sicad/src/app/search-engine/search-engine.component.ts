d0f46d46a05ffbe4241628d70858f4312e91d058
achicote <ranaranitaranon@hotmail.com>
2024-01-07 19:51:01 +0100

    export to pdf

--- UIAngular/Sicad/src/app/search-engine/search-engine.component.ts
+++ UIAngular/Sicad/src/app/search-engine/search-engine.component.ts
@@ -1,8 +1,11 @@
-import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {Item} from '../item';
 import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
+
 @Component({
   selector: 'app-search-engine',
   standalone: true,
@@ -15,10 +18,30 @@ export class SearchEngineComponent implements AfterViewInit{
   Object = Object;
   dataSource!: MatTableDataSource<any>;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild('miTabla') content!:ElementRef;
   ngOnInit() {
     this.dataSource = new MatTableDataSource<any>(this.list);
   }
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+  }
+  public SavePDF(){
+    let content=this.content.nativeElement;
+    let doc = new jsPDF();
+    autoTable(doc, { html: '#miTabla' })
+    // doc.save('my_table.pdf')
+    // let _elementHandlers =
+    // {
+    //   '#editor':function(element,renderer){
+    //     return true;
+    //   }
+    // };
+    // doc.fromHTML(content.innerHTML,15,15,{
+
+    //   'width':190,
+    //   'elementHandlers':_elementHandlers
+    // });
+
+    doc.save('test.pdf');
   }
 }
